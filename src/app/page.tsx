cd1fdfc178bd6a3dae318b1fe93e9fe27aa17c4c
angiekimm <eungg22@gmail.com>
2024-01-21 21:18:11 +0900

    initial setting

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>Angie&apos;s todo</div>
+        </main>
+    );
+}
